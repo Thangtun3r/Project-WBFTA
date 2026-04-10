65c545d2e2c0fa86545f15ceae243ca344582193
Thangtuner <voquangthang04@gmail.com>
2026-04-10 17:14:40 +0700

    some update for the visual

--- Assets/_Scripts/Player/Cursor.cs
+++ Assets/_Scripts/Player/Cursor.cs
@@ -1,32 +1,18 @@
-using System;
 using UnityEngine;
 
 public class MouseFollower : MonoBehaviour
 {
-    public float followSpeed = 50f; // Speed of following the mouse
-    private Rigidbody2D rb;
     public GameObject cursor;
 
     private void Start()
     {
-        rb = cursor.GetComponent<Rigidbody2D>();
-        rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous; // Prevents fast movement from clipping
-        rb.interpolation = RigidbodyInterpolation2D.Interpolate; // Smooth movement
+        UnityEngine.Cursor.visible = false;
     }
 
-    private void Update()
+    private void LateUpdate()
     {
-        Cursor.visible = false;
-         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        rb.MovePosition(Vector2.Lerp(rb.position, mousePosition, followSpeed * Time.fixedDeltaTime));
-
-        // Zero out velocity so collision force is always consistent
-        rb.linearVelocity = Vector2.zero;
-        rb.angularVelocity = 0f;
-    }
-
-    private void FixedUpdate()
-    {
-       
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = cursor.transform.position.z;
+        cursor.transform.position = mousePosition;
     }
 }
