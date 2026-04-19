233b80cd4af82b3335f94ed07907d1932c651f3c
wiwenid <voquangthang04@gmail.com>
2026-04-20 01:06:08 +0700

    Item type added

--- Assets/_Item System/Scripts/ItemDefinition.cs
+++ Assets/_Item System/Scripts/ItemDefinition.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
 
-
+public enum ItemType
+{
+    offensive,
+    defensive,
+    utility
+}
 [CreateAssetMenu(fileName = "New Item", menuName = "Item System/Item Definition")]
 public class ItemDefinition: ScriptableObject
 {
     public string itemName; 
+    public ItemType itemType;
     public Sprite icon;
     public string description;
     
